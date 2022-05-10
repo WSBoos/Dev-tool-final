2e9edbd214d84ce32f178624a8ab17cf4a21d6dd
unknown <boos.14768@gmail.com>
2022-05-10 17:53:35 +0700

    add: prometheus

--- back-end/index.js
+++ back-end/index.js
@@ -5,6 +5,23 @@ const facultyModel = require('./model/model.js');
 const app = express();
 const cors = require('cors')
 const port = 4000;
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+const { MeterProvider }  = require('@opentelemetry/sdk-metrics-base');
+
+// Add your port and startServer to the Prometheus options
+const options = {port: 9464, startServer: true};
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter('demo-prometheus');
+
+// Now, start recording data
+const counter = meter.createCounter('count_database', {
+  description: 'count database'
+});
 
 mongoose.Promise = global.Promise 
 mongoose.connect(configs.mongouri, {
@@ -16,6 +33,10 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/api/falcuties', async(req, res) => {
+    counter.add(1, { service_name: "facuties" });
+    counter.add(1, { service_name: "facuties", status: "success", path: "/api/falcuties" });
+    counter.add(1, { service_name: "facuties", status: "failure", path: "/api/falcuties" });
+    counter.add(1, { service_name: "facuties", status: "data not found", path: "/api/falcuties" });
     try {
        const result = await facultyModel.find()
        res.status(200).send(result)
